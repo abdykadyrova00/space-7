e5a5c430966668d86d04b8c87c1b9140982f6330
razi02 <raziabdykadyrkyzy245@gmail.com>
2023-07-03 16:00:14 +0600

    it-space

---
+++ src/page/flutter/flutter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Flutter = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Flutter;
---
+++ src/page/ux/ux.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ux = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Ux;
