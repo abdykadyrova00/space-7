ef5c500ed4d6494c5feb0d828fab209a1d8f1b88
cholponaiDev <cholponai0205@gmail.com>
2023-07-06 17:27:29 +0600

    footer

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from "./hero/hero";
 import Course from "./course/course";
+import Review from "./review/review";
+import About from "./about/about";
 
 const Home = () => {
     return (
         <div>
            <Hero/>
+            <About/>
             <Course/>
+            <Review/>
         </div>
     );
 };
